0244dc1f16bf4574218c3a23768f0d1ec25bf96a
jifon <orozbekova.kyzzhjibek@gmail.com>
2023-05-12 01:47:00 +0600

    fixed bug

--- src/main/java/kyzzhibek/orozbekova/encrypt/KyrgyzTextAnalyzerController.java
+++ src/main/java/kyzzhibek/orozbekova/encrypt/KyrgyzTextAnalyzerController.java
@@ -1,25 +1,43 @@
 package kyzzhibek.orozbekova.encrypt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
 
-@RestController
+//@RestController
+@Controller
 public class KyrgyzTextAnalyzerController {
 
     @Autowired
     private KyrgyzTextAnalyzerService kyrgyzTextAnalyzerService;
 
-    @GetMapping("/frequency-analysis")
-    public Map<Character, Integer> getFrequencyAnalysis() throws IOException {
-        return kyrgyzTextAnalyzerService.getFrequencyAnalysis();
+    @GetMapping({"/","/index"})
+    public String index() {
+        return "index";
     }
 
-    @PutMapping("/decrypt")
-    public String decrypt(@RequestBody String encryptedText) throws IOException {
-        return kyrgyzTextAnalyzerService.decrypt(encryptedText);
+    @PostMapping("/decrypt")
+    public String decrypt(@RequestParam String input, Model model) throws IOException {
+        // здесь нужно написать логику расшифровки текста
+        Request encryptedText = new Request(input);
+
+        String result = kyrgyzTextAnalyzerService.decrypt(encryptedText); // результат расшифровки
+        model.addAttribute("result", result);
+        return "index";
     }
+
+//    @GetMapping("/frequency-analysis")
+//    public Map<Character, Integer> getFrequencyAnalysis() throws IOException {
+//        return kyrgyzTextAnalyzerService.getFrequencyAnalysis();
+//    }
+//
+//    @PutMapping("/decrypt")
+//    public String decrypt(@RequestBody Request encryptedText) throws IOException {
+//        return kyrgyzTextAnalyzerService.decrypt(encryptedText);
+//    }
 }
 
--- src/main/java/kyzzhibek/orozbekova/encrypt/KyrgyzTextAnalyzerService.java
+++ src/main/java/kyzzhibek/orozbekova/encrypt/KyrgyzTextAnalyzerService.java
@@ -6,9 +6,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class KyrgyzTextAnalyzerService {
@@ -29,28 +27,83 @@ public class KyrgyzTextAnalyzerService {
         return frequencyMap;
     }
 
-    public String decrypt(String encryptedText) throws IOException {
-        Map<Character, Integer> frequencyMap = getFrequencyAnalysis();
+    public String decrypt(Request encryptedText) throws IOException {
+        Map<Character, Integer> encryptedFreqMap = getFrequencyAnalysis(encryptedText.getText().toLowerCase());
+        Map.Entry<Character, Integer>[] encryptedFreqArray = getSortedFrequencies(encryptedFreqMap);
+        Map.Entry<Character, Integer>[] kyrgyzFreqArray = getSortedFrequencies(getFrequencyAnalysis());
 
-        StringBuilder decryptedTextBuilder = new StringBuilder();
-        for (char c : encryptedText.toLowerCase().toCharArray()) {
-            if (ALPHABET.indexOf(c) != -1) {
-                Optional<Map.Entry<Character, Integer>> maxEntry = frequencyMap.entrySet()
-                        .stream()
-                        .filter(entry -> Character.toLowerCase(entry.getKey()) == c)
-                        .max(Map.Entry.comparingByValue());
-
-                if (maxEntry.isPresent()) {
-                    decryptedTextBuilder.append(maxEntry.get().getKey());
-                } else {
-                    decryptedTextBuilder.append(c);
-                }
-            } else {
-                decryptedTextBuilder.append(c);
-            }
+        System.out.println("encryptedFreqArray");
+        for (Map.Entry<Character, Integer> x : encryptedFreqArray){
+            System.out.println(x.getKey()+"  "+ x.getValue());
         }
 
-        return decryptedTextBuilder.toString();
+
+        System.out.println("kyrgyzFreqArray");
+        for (Map.Entry<Character, Integer> x : kyrgyzFreqArray){
+            System.out.println(x.getKey()+"  "+ x.getValue());
+        }
+
+
+        System.out.println("\n mapp");
+        Map<Character, Character> mapping = new HashMap<>();
+        for (int i = 0; i < encryptedFreqArray.length; i++) {
+            mapping.put(encryptedFreqArray[i].getKey(), kyrgyzFreqArray[i].getKey());
+            System.out.println( encryptedFreqArray[i].getKey()+ " - " +kyrgyzFreqArray[i].getKey());
+        }
+
+        StringBuilder decryptedText = new StringBuilder();
+        for (char c : encryptedText.getText().toLowerCase().toCharArray()) {
+            decryptedText.append(mapping.getOrDefault(c,c));
+
+        }
+
+        return decryptedText.toString();
     }
+
+    private Map<Character, Integer> getFrequencyAnalysis(String text) {
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+
+        for (char c : text.toLowerCase().toCharArray()) {
+            if (ALPHABET.indexOf(c) != -1) {
+                frequencyMap.merge(c, 1, Integer::sum);
+            }
+        }
+        return frequencyMap;
+    }
+    @SuppressWarnings("unchecked")
+    private Map.Entry<Character, Integer>[] getSortedFrequencies(Map<Character, Integer> frequencyMap) {
+        Map.Entry<Character, Integer>[] entries = frequencyMap.entrySet().toArray(new Map.Entry[0]);
+        Arrays.sort(entries, (a, b) -> b.getValue().compareTo(a.getValue()));
+        return entries;
+    }
+
+//    public String decrypt(Request encryptedText) throws IOException {
+//        Map<Character, Integer> frequencyMap = getFrequencyAnalysis();
+//
+//        System.out.println(encryptedText.getText());
+//        encryptedText.setText(encryptedText.getText().toLowerCase());
+//
+//        StringBuilder decryptedTextBuilder = new StringBuilder();
+//
+//        for (char c : encryptedText.getText().toCharArray()) {
+//            if (ALPHABET.indexOf(c) != -1) {
+//                Optional<Map.Entry<Character, Integer>> maxEntry = frequencyMap.entrySet()
+//                        .stream()
+//                        .filter(entry -> Character.toLowerCase(entry.getKey()) == c)
+//                        .max(Map.Entry.comparingByValue());
+//
+//                if (maxEntry.isPresent()) {
+//                    System.out.println(maxEntry.get().getKey());
+//                    decryptedTextBuilder.append(maxEntry.get().getKey());
+//                } else {
+//                    decryptedTextBuilder.append(c);
+//                }
+//            } else {
+//                decryptedTextBuilder.append(c);
+//            }
+//        }
+//
+//        return decryptedTextBuilder.toString();
+//    }
 }
 
